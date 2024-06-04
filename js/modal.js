2d35d5ef7a3b79f318f372570bc346813e99a036
emiaesthetic <emi.aesthetic@gmail.com>
2024-06-04 09:19:23 +0500

    Fix modal window closing

--- js/modal.js
+++ js/modal.js
@@ -1,33 +1,32 @@
 'use strict';
 
-const addOverlayActive = (overlay) => {
-  overlay.classList.add('overlay--active');
-};
+{
+  const addOverlayActive = (overlay) => {
+    overlay.classList.add('overlay--active');
+  };
 
-const removeOverlayActive = (overlay) => {
-  overlay.classList.remove('overlay--active');
-};
+  const removeOverlayActive = (overlay) => {
+    overlay.classList.remove('overlay--active');
+  };
 
-const init = () => {
-  const btnAddProduct = document.querySelector('.goods__add-product');
-  const overlay = document.querySelector('.overlay');
-  const btnClose = document.querySelector('.modal__close-button');
+  const init = () => {
+    const btnAddProduct = document.querySelector('.goods__add-product');
+    const overlay = document.querySelector('.overlay');
 
-  btnAddProduct.addEventListener('click', () => {
-    addOverlayActive(overlay);
-  });
+    btnAddProduct.addEventListener('click', () => {
+      addOverlayActive(overlay);
+    });
 
-  overlay.addEventListener('click', event => {
-    const target = event.target;
+    overlay.addEventListener('click', (event) => {
+      const target = event.target;
+      if (
+        target.classList.contains('overlay') ||
+        target.closest('.modal__close-button')
+      ) {
+        removeOverlayActive(overlay);
+      }
+    });
+  };
 
-    if (target.className.includes('overlay--active')) {
-      removeOverlayActive(overlay);
-    }
-  });
-
-  btnClose.addEventListener('click', () => {
-    removeOverlayActive(overlay);
-  });
-};
-
-init();
+  window.modalWindow = init;
+}
